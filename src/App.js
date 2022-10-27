9a4d2ef188a85998f87d003161e5df18d947fc2c
ismayil0999 <111593973+ismayil0999@users.noreply.github.com>
2022-10-27 12:08:14 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import Navbar from "./components/Navbar";
+import Category from './components/Category';
+import './App.css';
+import BottomMenu from './components/BottomMenu';
+import Card from './components/Card';
+import { Routes, Route} from "react-router-dom";
+import Yenielan from './components/Yenielan';
+import Comments from './components/Comments';
+import Texnologiya from "./components/Texnologiya";
+import Emlak from "./components/Emlak";
+import Neqliyyat from './components/Neqliyyat';
+import Iselani from "./components/Iselani"
+class App extends Component {
+  render() {
+    document.addEventListener("click", function (e) {
+     if(e.target.className=="search"){
+       e.target.addEventListener("keyup",function(){
+         var name=document.querySelectorAll(".product-name")
+         for(var i=0;i<=name.length;i++){
+           if(name[i].textContent.toLowerCase().startsWith(e.target.value)){
+             name[i].parentElement.parentElement.style.display=""
+           }else{
+            name[i].parentElement.parentElement.style.display="none"
+           }
+         }
+       })
+     }
+    })
+
+    return (
+      <div>
+        <Navbar />
+        <Category />
+        <BottomMenu />
+        <Routes>
+          <Route path="/" element={<Card/>}></Route>
+          <Route path='/yenielan'  element={<Yenielan />}></Route>
+          <Route path='/texnologiya' element={<Texnologiya />}></Route>
+          <Route path='/emlak'  element={<Emlak />}></Route>
+          <Route path='/neqliyyat'  element={<Neqliyyat />}></Route>
+          <Route path='/iselanlari'  element={<Iselani />}></Route>
+          <Route path='/comments/:id'  element={<Comments/>}></Route>
+        </Routes>
+      </div>
+    );
+  }
+}
+export default App;
