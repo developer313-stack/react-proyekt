dc59ceb94e316159ebaa1906b061f9f3e34c86ce
ismayil0999 <111593973+ismayil0999@users.noreply.github.com>
2022-10-31 15:34:29 +0300

    Add files via upload

---
+++ src/components/Favorite.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function Favorite() {
+    var products=JSON.parse(localStorage.getItem("ads",products))
+    return (
+        <div>
+           <div className='card-container'>
+          {products.map(vehicle=>{
+      return  <div className='card'>
+      <span id='icon' className='fav-icon'><i className="far fa-heart" id={vehicle.id}></i></span>
+      <div className='img-box'>
+      <img src={vehicle.imgsrc} alt=""></img>
+      <span className='vip-icon'><img src='./icons/vipicon.svg' alt=''></img></span>
+      </div>
+      <div className='card-body'>
+          <h4>{vehicle.priceval}</h4>
+          <p className='product-name'>{vehicle.productsname}</p>
+          <p className='date'>{vehicle.productscreated}</p>
+      </div>
+      </div>
+    })}
+    </div>
+        </div>
+    );
+}
+export default Favorite;
